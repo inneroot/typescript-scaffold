554856fe4103dd4a17749acd6f1ddf535d32ea81
inneroot <hakooplay@gmail.com>
2020-08-03 18:30:56 +0300

    express with TS

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello TypeScript')
+import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+app.get('/', (req, res) => res.json({message: 'Works'}))
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
